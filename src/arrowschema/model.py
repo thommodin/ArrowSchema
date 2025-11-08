45ed1ee568089248da468375b33fe82f2c2c56d6
thommodin <thomas.galindo@utas.edu.au>
2025-11-08 17:21:38 +1100

    implement pyarrow type

--- src/arrowschema/model.py
+++ src/arrowschema/model.py
@@ -1,8 +1,9 @@
-import arrowschema.literals
+import arrowschema.literal
 import pyarrow
 import pydantic
 import re
 import typing
+import warnings
 
 
 class PyarrowType(pydantic.BaseModel):
@@ -12,8 +13,20 @@ class PyarrowType(pydantic.BaseModel):
     Will probably be limited a to a subset to avoid extra complex types
     """
 
-    type: arrowschema.literals.pyarrow_integer
-    unit: arrowschema.literals.pyarrow_time_units | None = pydantic.Field(
+    type: (
+        arrowschema.literal.pyarrow_null
+        | arrowschema.literal.pyarrow_boolean
+        | arrowschema.literal.pyarrow_integer
+        | arrowschema.literal.pyarrow_float
+        | arrowschema.literal.pyarrow_string
+        | arrowschema.literal.pyarrow_binary
+        | arrowschema.literal.pyarrow_date
+        | arrowschema.literal.pyarrow_time
+        | arrowschema.literal.pyarrow_duration
+        | arrowschema.literal.pyarrow_month_day_nano_interval
+        | arrowschema.literal.pyarrow_timestamp
+    )
+    unit: arrowschema.literal.pyarrow_time_units | None = pydantic.Field(
         default=None,
         description="The units. Applies to time related types",
     )
@@ -24,56 +37,168 @@ class PyarrowType(pydantic.BaseModel):
     )
 
     @property
+    def alias(self) -> str:
+        """
+        The alias is a combination of the type and unit, if the unit exists
+
+        Returns: str
+        """
+        return f"{self.type}[{self.unit}]" if self.unit else self.type
+
+    @property
     def native(self):
         """
         Convert to a native pyarrow type
         """
 
-        # Extract the type function
-        pyarrow_type = getattr(pyarrow, self.type)
-
-        # Return the type with unit and timezone
-        if self.tz is not None:
-            return pyarrow_type(unit=self.unit, tz=self.timezone)
-
-        # Return the type with unit
-        elif self.unit is not None:
-            return pyarrow_type(
-                unit=self.unit,
+        # Timestamp special case
+        if self.type.startswith("timestamp"):
+            return pyarrow.timestamp(
+                self.unit,
+                tz=self.tz,
             )
 
-        # Return just the type
+        # Otherwise use the type alias
         else:
-            return pyarrow_type()
+            return pyarrow.type_for_alias(self.alias)
 
     @classmethod
     def from_pyarrow(cls, native_pyarrow_type) -> typing.Self:
         """
         Convert from a native pyarrow type instantiation to a PyarrowType
-        """
 
+        `https://github.com/apache/arrow/blob/cd23a765442bdbaaef43d0e4b239094fb01e37ae/python/pyarrow/types.pxi#L5761C11-L5761C24`
+        """
         # Extract datatype elements
         pattern = (
-            # The type
+            # Type
             r"^(\w+)"
-            # The units (if exists)
-            r"|(?:\[(day|s|ms|us|ns)"
-            # The timezone (if exists)
-            r"|(?:,\s*tz=)?(([\+\-]\d{1,2}:\d{2}|[Zz]))?\])?$"
+            # Unit
+            r"(?:\[(day|s|ms|ns|us)"
+            # TZ
+            r"(?:,\s*tz=([\+\-]\d{1,2}:\d{2}|[Zz]))?\])?"
         )
         match = re.match(pattern, str(native_pyarrow_type))
-        if isinstance(match, re.Match):
-            pyarrow_datatype = match.group(1)
-            pyarrow_unit = match.group(2)
-            pyarrow_timezone = match.group(3)
-        else:
-            raise ValueError(f"incompatible field type: {native_pyarrow_type}")
+        type_ = match.group(1)
+        unit = match.group(2)
+        tz = match.group(3)
 
-        return cls(
-            type=pyarrow_datatype,
-            unit=pyarrow_unit,
-            timezone=pyarrow_timezone,
-        )
+        match type_:
+            # Null
+            case "null":
+                return cls(type=type_)
+
+            # Boolean
+            case "bool":
+                return cls(type=type_)
+
+            # Integer
+            case (
+                "int8"
+                | "int16"
+                | "int32"
+                | "int64"
+                | "uint8"
+                | "uint16"
+                | "uint32"
+                | "uint64"
+            ):
+                return cls(type=type_)
+
+            # Float
+            case "float16" | "halffloat" | "float32" | "float" | "float64" | "double":
+                return cls(type=type_)
+
+            # String
+            case "utf8" | "string" | "large_string" | "large_utf8" | "string_view":
+                return cls(type=type_)
+
+            # String
+            case "binary" | "large_binary" | "binary_view":
+                return cls(type=type_)
+
+            # Date
+            case "date32" | "date64":
+                return cls(
+                    type=type_,
+                    unit=unit,
+                )
+
+            # Time
+            case "time32" | "time64":
+                return cls(
+                    type=type_,
+                    unit=unit,
+                )
+
+            # Duration
+            case "duration":
+                return cls(
+                    type=type_,
+                    unit=unit,
+                )
+
+            # Month Day Nano Interval
+            case "month_day_nano_interval":
+                return cls(type=type_)
+
+            # Timestamp
+            case "timestamp":
+                return cls(
+                    type=type_,
+                    unit=unit,
+                    tz=tz,
+                )
+
+            case _:
+                raise NotImplementedError(f"`{native_pyarrow_type}` not implemented")
+
+    @pydantic.model_validator(mode="after")
+    def check_time_type_unit_and_timezone_dependencies(self) -> typing.Self:
+        """
+        Validate the required units for time types
+        are in place
+        """
+        if self.type.startswith("time32"):
+            if self.unit not in {"s", "ms"}:
+                raise ValueError(
+                    f"time32 requires units `s` or `ms`, found: `{self.unit}`"
+                )
+
+        elif self.type.startswith("time64"):
+            if self.unit not in {"us", "ns"}:
+                raise ValueError(
+                    f"time64 requires units `us` or `ns`, found: `{self.unit}`"
+                )
+
+        elif self.type.startswith("date32"):
+            if self.unit != "day":
+                raise ValueError(f"date32 requires units `day`, found: `{self.unit}`")
+
+        elif self.type.startswith("date64"):
+            if self.unit != "ms":
+                raise ValueError(f"date64 requires units `ms`, found: `{self.unit}`")
+
+        elif self.type.startswith("timestamp"):
+            if self.unit not in {"s", "ms", "us", "ns"}:
+                raise ValueError(
+                    f"timestamp requires units `s`, `ms`, `us` or `ns`, found: `{self.unit}`"
+                )
+            elif self.tz is None:
+                raise ValueError("timestamp requires a tz")
+
+        elif self.type.startswith("duration"):
+            if self.unit not in {"s", "ms", "us", "ns"}:
+                raise ValueError(
+                    f"duration requires units `s`, `ms`, `us` or `ns`, found: `{self.unit}`"
+                )
+        else:
+            if self.unit is not None or self.tz is not None:
+                warnings.warn(
+                    f"unit of type `{self.type}` should not have a unit or timezone specified (unit=`{self.unit}`, timezone=`{self.unit})"
+                )
+
+        return self
 
 
 class PyarrowField(pydantic.BaseModel):
--- test/test_pyarrow_type.py
+++ test/test_pyarrow_type.py
@@ -1,6 +1,7 @@
 import arrowschema.model
-import arrowschema.literals
+import arrowschema.literal
 import pyarrow
+import re
 import typing
 
 
@@ -15,26 +16,93 @@ def _test_simple_type(
         pyarrow_type = arrowschema.model.PyarrowType(
             type=pyarrow_literal,
         )
+        pyarrow_type.native
 
         # Check the native hydrated correctly
-        assert (
-            getattr(
-                pyarrow,
-                pyarrow_literal,
-            )()
-            is pyarrow_type.native
-        )
+        assert pyarrow.type_for_alias(pyarrow_literal).equals(pyarrow_type.native)
 
     # Hydrate and test all native pyarrow types to type
     for pyarrow_type_ in pyarrow_types:
         pyarrow_type = arrowschema.model.PyarrowType.from_pyarrow(pyarrow_type_)
+        assert pyarrow_type_.equals(pyarrow_type.native)
 
-        assert pyarrow_type_ is pyarrow_type.native
+
+def _test_unit_type(
+    literal: typing.Type,
+    pyarrow_types: list,
+):
+    # Hydrate and test all type to native calls for pyarrow integers
+    pyarrow_literals = list(typing.get_args(literal))
+
+    # Filter to literals with units
+    for pyarrow_literal in [
+        pyarrow_literal
+        for pyarrow_literal in pyarrow_literals
+        if re.match(
+            pattern=r"(\w+)\[.*\]",
+            string=pyarrow_literal,
+        )
+        is not None
+    ]:
+        # Extract type and unit
+        match = re.match(
+            # Extract datatype elements
+            pattern=(
+                # Type
+                r"^(\w+)"
+                # Unit
+                r"(?:\[(day|s|ms|ns|us)\])"
+                # TZ
+                # r"(?:,\s*tz=([\+\-]\d{1,2}:\d{2}|[Zz]))?\])?"
+            ),
+            string=pyarrow_literal,
+        )
+        type_ = match.group(1)
+        unit = match.group(2)
+        tz = "z" if type_ == "timestamp" else None
+
+        pyarrow_type = arrowschema.model.PyarrowType(
+            type=type_,
+            unit=unit,
+            tz=tz,
+        )
+
+        match type_:
+            # Date 32 has units but only one for 32 and 64, so does not take them
+            case "date32" | "date64":
+                assert pyarrow.type_for_alias(type_).equals(pyarrow_type.native)
+
+            # Time related with units
+            case "time32" | "time64" | "duration" | "month_day_nano_interval":
+                assert getattr(pyarrow, type_)(unit).equals(pyarrow_type.native)
+
+    # Hydrate and test all native pyarrow types to type
+    for pyarrow_type_ in pyarrow_types:
+        pyarrow_type = arrowschema.model.PyarrowType.from_pyarrow(pyarrow_type_)
+        assert pyarrow_type_.equals(pyarrow_type.native)
 
 
-def test_simple_types():
+def test_null_type():
     _test_simple_type(
-        literal=arrowschema.literals.pyarrow_integer,
+        literal=arrowschema.literal.pyarrow_null,
+        pyarrow_types=[
+            pyarrow.null(),
+        ],
+    )
+
+
+def test_bool_type():
+    _test_simple_type(
+        literal=arrowschema.literal.pyarrow_boolean,
+        pyarrow_types=[
+            pyarrow.bool_(),
+        ],
+    )
+
+
+def test_integer_types():
+    _test_simple_type(
+        literal=arrowschema.literal.pyarrow_integer,
         pyarrow_types=[
             pyarrow.int8(),
             pyarrow.int16(),
@@ -44,5 +112,95 @@ def test_simple_types():
             pyarrow.uint16(),
             pyarrow.uint32(),
             pyarrow.uint64(),
+        ],
+    )
+
+
+def test_float_types():
+    _test_simple_type(
+        literal=arrowschema.literal.pyarrow_integer,
+        pyarrow_types=[
+            pyarrow.float16(),
+            pyarrow.float32(),
+            pyarrow.float64(),
+        ],
+    )
+
+
+def test_string_types():
+    _test_simple_type(
+        literal=arrowschema.literal.pyarrow_string,
+        pyarrow_types=[
+            pyarrow.string(),
+            pyarrow.utf8(),
+            pyarrow.large_string(),
+            pyarrow.large_utf8(),
+            pyarrow.string_view(),
+        ],
+    )
+
+
+def test_binary_types():
+    _test_simple_type(
+        literal=arrowschema.literal.pyarrow_binary,
+        pyarrow_types=[
+            pyarrow.binary(),
+            pyarrow.large_binary(),
+            pyarrow.binary_view(),
+        ],
+    )
+
+
+def test_date_types():
+    _test_unit_type(
+        literal=arrowschema.literal.pyarrow_date,
+        pyarrow_types=[
+            pyarrow.date32(),
+            pyarrow.date64(),
+        ],
+    )
+
+
+def test_time_types():
+    _test_unit_type(
+        literal=arrowschema.literal.pyarrow_time,
+        pyarrow_types=[
+            pyarrow.time32("s"),
+            pyarrow.time32("ms"),
+            pyarrow.time64("us"),
+            pyarrow.time64("ns"),
+        ],
+    )
+
+
+def test_duration_types():
+    _test_unit_type(
+        literal=arrowschema.literal.pyarrow_duration,
+        pyarrow_types=[
+            pyarrow.duration("s"),
+            pyarrow.duration("ms"),
+            pyarrow.duration("us"),
+            pyarrow.duration("ns"),
+        ],
+    )
+
+
+def test_month_day_nano_interval_type():
+    _test_unit_type(
+        literal=arrowschema.literal.pyarrow_month_day_nano_interval,
+        pyarrow_types=[
+            pyarrow.month_day_nano_interval(),
+        ],
+    )
+
+
+def test_timestamp_type():
+    _test_unit_type(
+        literal=arrowschema.literal.pyarrow_timestamp,
+        pyarrow_types=[
+            pyarrow.timestamp("s", tz="z"),
+            pyarrow.timestamp("ms", tz="z"),
+            pyarrow.timestamp("ns", tz="z"),
+            pyarrow.timestamp("us", tz="z"),
         ],
     )
