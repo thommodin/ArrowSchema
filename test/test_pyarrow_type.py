559408dada40b4ac800d772f1125387e575f63f7
thommodin <thomas.galindo@utas.edu.au>
2025-11-08 11:38:53 +1100

    make simple type test generic

--- test/test_pyarrow_type.py
+++ test/test_pyarrow_type.py
@@ -4,38 +4,45 @@ import pyarrow
 import typing
 
 
-def test_int():
+def _test_simple_type(
+    literal: typing.Type,
+    pyarrow_types: list,
+):
     # Hydrate and test all type to native calls for pyarrow integers
-    pyarrow_integer_literals = list(
-        typing.get_args(arrowschema.literals.pyarrow_integer)
-    )
+    pyarrow_literals = list(typing.get_args(literal))
 
-    for literpyarrow_integer_literal in pyarrow_integer_literals:
+    for pyarrow_literal in pyarrow_literals:
         pyarrow_type = arrowschema.model.PyarrowType(
-            type=literpyarrow_integer_literal,
+            type=pyarrow_literal,
         )
 
         # Check the native hydrated correctly
         assert (
             getattr(
                 pyarrow,
-                literpyarrow_integer_literal,
+                pyarrow_literal,
             )()
             is pyarrow_type.native
         )
 
-    # Hydrate and test all native to type calls for pyarrow integers
-    pyarrow_integer_types = [
-        pyarrow.int8(),
-        pyarrow.int16(),
-        pyarrow.int32(),
-        pyarrow.int64(),
-        pyarrow.uint8(),
-        pyarrow.uint16(),
-        pyarrow.uint32(),
-        pyarrow.uint64(),
-    ]
-    for pyarrow_integer_type in pyarrow_integer_types:
-        pyarrow_type = arrowschema.model.PyarrowType.from_pyarrow(pyarrow_integer_type)
+    # Hydrate and test all native pyarrow types to type
+    for pyarrow_type_ in pyarrow_types:
+        pyarrow_type = arrowschema.model.PyarrowType.from_pyarrow(pyarrow_type_)
 
-        assert pyarrow_integer_type is pyarrow_type.native
+        assert pyarrow_type_ is pyarrow_type.native
+
+
+def test_simple_types():
+    _test_simple_type(
+        literal=arrowschema.literals.pyarrow_integer,
+        pyarrow_types=[
+            pyarrow.int8(),
+            pyarrow.int16(),
+            pyarrow.int32(),
+            pyarrow.int64(),
+            pyarrow.uint8(),
+            pyarrow.uint16(),
+            pyarrow.uint32(),
+            pyarrow.uint64(),
+        ],
+    )
